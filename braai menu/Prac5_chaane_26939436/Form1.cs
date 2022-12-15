2ec4936ada34816a5f6088b635106fef1e2e6606
chaane <49711533+chaane@users.noreply.github.com>
2022-12-15 11:41:20 +0200

    Add files via upload

---
+++ braai menu/Prac5_chaane_26939436/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prac5_chaane_26939436
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Green;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.White;  
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            this.BackColor= System.Drawing.Color.White;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Quantitiy;
+            
+            Quantitiy =int.Parse( textBox1.Text);
+
+            if ( radioButton1.Checked==true)
+            {
+                MessageBox.Show(textBox1.Text +" Pap and wors orders are in their way");
+                return;
+            }
+            else if( radioButton2.Checked == true)
+            {
+                MessageBox.Show(textBox1.Text +" Delicious Fillet Steak with roast veggies coming up" );
+                return;
+            }
+            else if ( radioButton3.Checked == true)
+            {
+                MessageBox.Show(textBox1.Text +" Of your Chicken and chips on his way");
+                return;
+            }
+           
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != ',')
+                e.Handled = true;
+        }
+    }
+}
